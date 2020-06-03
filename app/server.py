ad3114a5222f538dbaaaab001021b3478fcd1013
GXE <gxedwards@gmail.com>
2020-06-03 15:05:27 -0400

    Change to my model

--- app/server.py
+++ app/server.py
@@ -8,9 +8,9 @@ from io import BytesIO
 from fastai import *
 from fastai.vision import *
 
-model_file_url = 'https://www.dropbox.com/s/y4kl2gv1akv7y4i/stage-2.pth?raw=1'
+model_file_url = 'https://drive.google.com/uc?export=download&id=1GGAgZ9ckV2sGmHP_CkRHTRyWIfn8WXUM'
 model_file_name = 'model'
-classes = ['black', 'grizzly', 'teddys']
+classes = ['bf109', 'p51', 'spitfire']
 path = Path(__file__).parent
 
 app = Starlette()
